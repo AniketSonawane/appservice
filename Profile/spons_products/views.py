6eabba566793dff9fe9d061a7b2e4d64b053435f
Sonawane <aniket.sonawane@stratacent.com>
2023-02-08 17:56:30 +0530

    first commit

---
+++ Profile/spons_products/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+from .models import products
+
+
+from .forms import ProductForm,RawProductForms
+
+
+# Create your views here.
+def product_detail_view(request):
+    obj = products.objects.get(id=1)
+    # context = {
+    #     'title' : obj.title,
+    #     'description' : obj.description
+    # }
+    context = {
+        'object' : obj
+    }
+    return render(request,'product_view.html',context)
+
+def product_create_view(request):
+     form = ProductForm(request.POST or None)
+     if form.is_valid():
+         form.save()
+         form = ProductForm()
+
+
+     context = {
+         'form' : form
+     }
+     return render(request,'product_create.html',context)
